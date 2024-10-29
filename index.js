99b49761b947c520275249d7db41bccba2d4915f
Kelbaker <kelbaker56@gmail.com>
2024-10-29 16:33:52 -0300

    Rota GET puro HTTP

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
  
+import http from 'http'
+
+const port = 3000
+const grades = [
+    {
+        studentName: "Thiago",
+        subject: "English",
+        grade: "8",
+    },
+];
+
+const server = http.createServer((request, response) => {
+    const {method, url} = request 
+
+    if (url === '/grades' && method === 'GET') {
+        response.writeHead(200, {'Content-Type': 'application/json'})
+        response.end(JSON.stringify(grades))
+    } else {
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({ message: 'Route not found'}));
+    }
+})
+
+server.listen(port, () => {
+    console.log(`Sever running on port ${port}`);
+})
