2786d059e16206534d9e8160c90008114e1ab3aa
Kelbaker <kelbaker56@gmail.com>
2024-10-29 17:45:29 -0300

    GET and POST inserted

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
  
 import http from 'http'
+import {v4} from "uuid"
 
 const port = 3000
 const grades = [
@@ -12,14 +13,30 @@ const grades = [
 
 const server = http.createServer((request, response) => {
     const {method, url} = request 
+    let body = '';
 
-    if (url === '/grades' && method === 'GET') {
-        response.writeHead(200, {'Content-Type': 'application/json'})
-        response.end(JSON.stringify(grades))
-    } else {
-        res.writeHead(404, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({ message: 'Route not found'}));
-    }
+    request.on('data', chunk => {
+        body += chunk.toString();
+    })
+
+    request.on('end', () => {
+        if (url === '/grades' && method === 'GET') { //API
+            response.writeHead(200, {'Content-Type': 'application/json'})
+            response.end(JSON.stringify(grades))
+        } else if (url === '/grades' && method === 'POST') {
+            const {studentName, subject, grade } = JSON.parse(body);
+            const newGrade = {id: v4() , studentName, subject, grade}
+            grades.push(newGrade);
+            res.writeHead(201, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(newGrade));
+        } else {
+            res.writeHead(404, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({ message: 'Route not found'}));
+        }
+
+    })
+
+    
 })
 
 server.listen(port, () => {
